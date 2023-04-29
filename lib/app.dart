647d4d44357bed97af36b39cd1e7a78677a28701
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-04-29 10:22:53 +0800

    :sparkles: Add zoom button, data toggle button etc

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,10 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:jakim_zones_map/locations/location_coordinate.dart';
 import 'package:latlong2/latlong.dart';
 
+import 'locations/location_coordinate_model.dart';
+
+enum PlaceData { azanPro, jakim }
+
 class App extends StatefulWidget {
   const App({super.key});
 
@@ -13,22 +17,27 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final MapController _mapController = MapController();
+  late List<LocationCoordinateData> _locationDatas;
+
+  PlaceData _placeData = PlaceData.azanPro;
+  double _zoomValue = 6.5;
+
+  @override
+  void initState() {
+    super.initState();
+    _locationDatas = _placeData == PlaceData.azanPro
+        ? LocationCoordinate.azanProZones
+        : LocationCoordinate.jakimZones;
+  }
+
   Color _getBgColor(String input) {
+    // generate random colour for each state
     Random random = Random(input.hashCode);
-    List<Color> colors = [
-      Colors.red,
-      Colors.green,
-      Colors.blue,
-      Colors.yellow,
-      Colors.orange,
-      Colors.purple,
-      Colors.pink,
-      Colors.teal,
-      Colors.cyan,
-      Colors.indigo,
-    ];
-    int index = random.nextInt(colors.length);
-    return colors[index];
+    int r = random.nextInt(256);
+    int g = random.nextInt(256);
+    int b = random.nextInt(256);
+    return Color.fromARGB(255, r, g, b);
   }
 
   Color getContrastingTextColor(Color color) {
@@ -40,15 +49,64 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
+      mapController: _mapController,
       options: MapOptions(
-        // center: LatLng(3.1476, 101.6962),
-        zoom: 9.5,
+        center: LatLng(4.92, 108.23),
+        zoom: _zoomValue,
+        minZoom: 5,
+        maxZoom: 15,
+        onMapEvent: (MapEvent mapEvent) {
+          if (mapEvent is MapEventScrollWheelZoom) {
+            setState(() => _zoomValue = mapEvent.zoom);
+          }
+        },
       ),
       nonRotatedChildren: [
         AttributionWidget.defaultWidget(
           source: 'OpenStreetMap contributors',
           onSourceTapped: null,
         ),
+        Positioned(
+          bottom: 20,
+          right: 10,
+          child: SizedBox(
+            width: 275,
+            child: Slider(
+                label: "Zoom",
+                value: _zoomValue,
+                max: 15,
+                min: 5,
+                onChanged: (double newZoomValue) {
+                  setState(() => _zoomValue = newZoomValue);
+                  _mapController.move(_mapController.center, newZoomValue);
+                }),
+          ),
+        ),
+        Positioned(
+          bottom: 40,
+          left: 10,
+          child: ElevatedButton(
+              onPressed: () {
+                if (_placeData == PlaceData.azanPro) {
+                  setState(() {
+                    _placeData = PlaceData.jakim;
+                    _locationDatas = LocationCoordinate.jakimZones;
+                  });
+                } else {
+                  setState(() {
+                    _placeData = PlaceData.azanPro;
+                    _locationDatas = LocationCoordinate.azanProZones;
+                  });
+                }
+              },
+              child: Text(_placeData.name.toUpperCase())),
+        ),
+        Positioned(
+            bottom: 20,
+            left: 12,
+            child: Text(
+              "${_locationDatas.length} locations",
+            ))
       ],
       children: [
         TileLayer(
@@ -57,7 +115,7 @@ class _AppState extends State<App> {
         ),
         MarkerLayer(
           markers: [
-            for (var loc in LocationCoordinate.azanProZones)
+            for (var loc in _locationDatas)
               Marker(
                 point: LatLng(loc.lat!, loc.lng!),
                 width: 80,
